f999364e6a20e0ed78b9d0159f9931a38f350bcd
Franco235 <57547815+Franco235@users.noreply.github.com>
2021-09-15 15:33:15 -0300

    creating the form and the error message

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+
 
 function App() {
   return (
-    <h1>Consulta de clima</h1>
+    <Fragment>
+      <Header
+        titulo='Clima React App'
+      />
+
+      <div className="contenedor-form">
+          <div className="container">
+              <div className="row">
+                  <div className="col m6 s12">
+                      <Formulario />
+                  </div>
+                  <div className="col m6 s12">
+                      2
+                  </div>
+              </div>
+          </div>
+      </div>
+
+    </Fragment>
   );
 }
 
